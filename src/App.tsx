cebdd6fa6829cf833b67f3a0ea066d494ca47214
sho6ix <shota09060906@gmail.com>
2022-11-07 16:40:11 +0900

    set analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import {
-  ChakraProvider
-} from "@chakra-ui/react"
-import theme from "./theme/theme"
-import { Home } from "./components/Home"
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme/theme";
+import { Home } from "./components/Home";
 
+import ReactGA from "react-ga4";
+import { useEffect } from "react";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Home/>
-  </ChakraProvider>
-)
+export const App = () => {
+
+  useEffect(() => {
+    ReactGA.initialize("G-78ENXXDB7V");
+    ReactGA.send("pageview");
+  }, []);
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Home />
+    </ChakraProvider>
+  );
+};
